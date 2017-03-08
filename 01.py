9f3d9157136b664f3feb206a57e986471572063a
mlnchk <danil.mlnchk@gmail.com>
2017-03-08 19:00:47 +0300

    beta

--- 01.py
+++ 01.py
@@ -1,9 +1,6 @@
 import requests
 import json
 
-api_url = 'https://api.vk.com/method/'
-# client_id = 5899797
-
 
 def make_news_list(api_url):
     news_method = 'newsfeed.search'
@@ -13,9 +10,16 @@ def make_news_list(api_url):
     }
     all_news = json.loads(requests.get(api_url + news_method, params).text)
     news_list = []
+    stop_words = ['бесплатн', 'скидк', 'помощ', 'промо']
     for news in all_news['response']:
+        flag = True
         if type(news) == dict:
-            news_list.append(news)
+            for stop_word in stop_words:
+                if news['text'].lower().count(stop_word) != 0:
+                    flag = False
+            if flag:
+                news_list.append(news)
+                # print(news['text'].lower().count)
     return news_list
 
 
@@ -23,5 +27,7 @@ def write_to_file(news_list):
     with open('news_base.json', 'w', encoding='utf8') as file:
         json.dump(news_list, file, ensure_ascii=False, indent=2)
 
-write_to_file(make_news_list(api_url))
-print('eee')
+if __name__ == '__main__':
+    api_url = 'https://api.vk.com/method/'
+    write_to_file(make_news_list(api_url))
+    print('Done!')
--- bot.py
+++ bot.py
@@ -1,10 +1,6 @@
-import requests
 import json
 import random
-import os
-from telegram.ext import Updater
-from telegram.ext import CommandHandler
-from telegram.ext import MessageHandler, Filters
+from telegram.ext import Updater, CommandHandler
 
 
 def start(bot, update):
@@ -15,21 +11,14 @@ def echo(bot, update):
     bot.sendMessage(chat_id=update.message.chat_id, text=update.message.text)
 
 
-def message(bot, update):
-    bot.sendMessage(chat_id=update.message.chat_id, text="Sorry, I didn't understand that command.")
-
-
 def news(bot, update):
-    news_dict = json.load(open('news_base.json'))
-    text = news_dict[random.randint(0, len(news_dict))]['text']
+    news_dict = json.load(open('news_base2.json'))
+    new = news_dict[random.randint(0, len(news_dict))]
+    text = '%s...\n\nhttps://vk.com/wall%s_%s' % (new['text'][0:100], new['owner_id'], new['id'])
     bot.sendMessage(chat_id=update.message.chat_id, text=text)
 
 
-
-
 token = '344289277:AAFW7MqEyssWyinS39XuEJz8F2wjBfZJtqk'
-method = ''
-url = 'https://api.telegram.org/bot' + token + method
 
 updater = Updater(token=token)
 dispatcher = updater.dispatcher
@@ -39,12 +28,5 @@ start_handler = CommandHandler('start', start)
 dispatcher.add_handler(start_handler)
 updater.start_polling()
 
-# echo_handler = MessageHandler(Filters.text, echo)
-# dispatcher.add_handler(echo_handler)
-
-message_handler = CommandHandler('message', message)
-dispatcher.add_handler(message_handler)
-
 news_handler = CommandHandler('news', news)
 dispatcher.add_handler(news_handler)
-
